db93a4cf2febc5df6120b077f557b54bfa6312c6
MariaJoseGarrido <mjgarrido.h@gmail.com>
2020-08-19 13:11:44 -0400

    zona padres

--- src/lib/view/templateParents.js
+++ src/lib/view/templateParents.js
@@ -2,15 +2,15 @@ export const parents = () => {
   const divParents = document.createElement('div');
 
   const viewParents = `
-  <div class="templateParents">
-    <div>
+  <div>
+    <div class="templateParents">
       <h2>Módulo Introductorio</h2>
       <h3>Tema 1: ¿Qué está pasando?</h3>
       <a href="./Doc/M1/HH_GanemosleAlVirus_PDF_M1_T1.pdf" download="">Descargar Material </a>
       <h3>Tema 2: ¿Qué es el covid-19?</h3>
       <a href="./Doc/M1/HH_GanemosleAlVirus_PDF_M1_T2.pdf" download="">Descargar Material </a>
     </div>
-    <div>  
+    <div class="templateParents">  
       <h2>Módulo 2: "Cultivando Nuestras Emociones"</h2>
       <h3>Tema 1: Cuidando las relaciones en cuarentena</h3>
       <a href="./Doc/M2/HH_GanemosleAlVirus_PDF_M2_T1.pdf/" download="">Descargar Material </a>
@@ -19,7 +19,7 @@ export const parents = () => {
       <h3>Tema 3: Psicohigiene: El cuidado de uno mismo y de los demás</h3>
       <a href="./Doc/M2/HH_GanemosleAlVirus_PDF_M2_T3.pdf/" download="">Descargar Material </a>
     </div>
-    <div> 
+    <div class="templateParents"> 
       <h2>Módulo 3: "Cultivando Nuestro Cuerpo"</h2>
       <h3>Alimentos</h3>
       <a href=".Doc/M3/HootieHoo_GAV_M3_Contenido.docx" download="">Descargar Material </a>
